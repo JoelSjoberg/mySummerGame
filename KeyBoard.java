1dc023b7e017ab09bc1ab239d79091dee792cfe0
Joel Sjöberg <joelsjoberg3@gmail.com>
2017-07-23 18:14:15 +0300

    keyBoard 1.01
    Keyboard is needed by the View class and the gameCore(which embodies the main game). Provides an array of booleans which describe which keys are pressed

---
+++ KeyBoard.java
@@ -0,0 +1,23 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoard implements KeyListener{
+
+	public static boolean key[] = new boolean[127];
+	
+	public void keyPressed(KeyEvent k) {
+		key[k.getKeyCode()] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent k) {
+		key[k.getKeyCode()] = false;
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		
+	}
+
+}
